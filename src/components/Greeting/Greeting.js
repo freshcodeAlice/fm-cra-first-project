810a72ef7601e676f979f3dcf448e86f90ca4ca8
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-29 12:01:33 +0300

    state and props

---
+++ src/components/Greeting/Greeting.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+
+class Greeting extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    
+    render() {
+        return <h1>Hello, {this.props.name}</h1>
+    }
+}
+
+export default Greeting;
+
+/*
+ * Доробити компоненту таким чином, щоб за натиснення на елемент текст "Hello" змінювався на "Bye!"
+ * 
+ * */
