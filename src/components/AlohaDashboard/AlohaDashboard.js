d2404daf03a4f1ea53ef0e233dd6a7d2a7b92ca2
freshcodeAlice <freshjs.ofte@gmail.com>
2022-09-30 17:07:11 +0300

    fix

--- src/components/AlohaDashboard/AlohaDashboard.js
+++ src/components/AlohaDashboard/AlohaDashboard.js
@@ -32,30 +32,31 @@ class AlohaDashboard extends React.Component {
     
     userToAloha() {
         const {usersArray} = this.state;
-        return usersArray.map((user)=> <Aloha key={user.id} name={user.firstName} lastName={user.lastName}/>);
+        return usersArray.map((user)=> <li key={user.id} name={user.firstName} lastName={user.lastName}>text <button data-id={user.id}>X</button> </li>);
     }
     
-    sortUsers() {
-        const {usersArray, sortDirection} = this.state;
-        const newArray = [...usersArray];
-        newArray.sort((a,b)=> {
-            if(sortDirection) {
-                return a.firstName > b.firstName ? 1 : -1
-            } else {
-                return a.firstName > b.firstName ? -1 : 1
-            }
-        });
-        this.setState({
-            usersArray: newArray,
-            sortDirection: !sortDirection
-        })
+    sortUsers(event) {
+        console.log(event);
+//         const {usersArray, sortDirection} = this.state;
+//         const newArray = [...usersArray];
+//         newArray.sort((a,b)=> {
+//             if(sortDirection) {
+//                 return a.firstName > b.firstName ? 1 : -1
+//             } else {
+//                 return a.firstName > b.firstName ? -1 : 1
+//             }
+//         });
+//         this.setState({
+//             usersArray: newArray,
+//             sortDirection: !sortDirection
+//         })
     }
     
    render() {
         
        return (
            <>
-           <button onClick={()=>{this.sortUsers()}}>Sort</button>
+           <button onClick={(event)=>{this.sortUsers(event)}}>Sort</button>
            {this.userToAloha()}
            </>
        )
