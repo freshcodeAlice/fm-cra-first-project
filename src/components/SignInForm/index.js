6979fa7a38c27393338741df4e5f24f8a710c6a8
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-05 09:58:39 +0300

    Controlled components (form)

---
+++ src/components/SignInForm/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './style.css';
+
+class SignInForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            pass: '',
+            isEmailValid: true
+        }
+    }
+
+    universalChangeHandler = ({target: {value, name}}) => {
+        this.setState({
+            [name]: value
+        })
+    }
+
+
+    submitHandler = (event) => {
+        event.preventDefault();
+
+        if (!this.state.email.includes('@')) {
+            this.setState({
+                isEmailValid: false
+            })
+        }
+        console.log(this.state);
+    }
+
+    render() {
+        const {email, pass, isEmailValid} = this.state;
+    return (
+        <form className='form-wrapper flex-column' onSubmit={this.submitHandler}>
+            <label className='flex-column'> Your email
+                <input type='text' placeholder='test@mail.com' name='email' onChange={this.universalChangeHandler} value={email} className={isEmailValid ? '' : 'invalid'}/>
+            </label>
+            <label className='flex-column'> Your password
+                <input type='text' name='pass' onChange={this.universalChangeHandler} value={pass}/>
+            </label>
+            <button type='submit'>Submit form</button>
+        </form>
+    )
+    }
+}
+
+export default SignInForm;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import UserList from './components/UserList/UserList';
-// import CounterPage from './components/Counter/CounterPage';
-// import App from './App';
-// import AlohaDashboard from './components/AlohaDashboard/AlohaDashboard'
-// import ToDoList from './components/ToDoList';
-// import Scene from './components/Scene/Scene';
+import App from './components/App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <UserList />
-    // <ToDoList />
-    // <AlohaDashboard />
-    // <Scene />
-    // <CounterPage />
+    <App />
 );
