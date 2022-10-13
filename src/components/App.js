3dc7feee6c2f1ed075652beb397ec09aaa6ff40a
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-13 09:52:27 +0300

    Navigator, clipboard

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,15 @@ function App() {
  
     return (
         <>
-        <button onClick={()=>{window.close()}}>Close</button>
-        <button onClick={()=>{window.open('http://localhost:3000/docs/index.html')}}>Open</button>
+        <button onClick={()=>{window.location.assign('https://www.w3schools.com/default.asp')}}>Assign</button>
+        <button onClick={()=>{window.location.reload()}}>Reload</button>
+        <button onClick={()=>{window.location.replace('https://developer.mozilla.org/ru/')}}>Replace</button>
+       <p>lorem ipsum dolor sit amet</p>
+       <button onClick={()=>{window.navigator.clipboard.writeText('lorem ipsum dolor sit amet')}} >Click to copy text</button>
+       <button onClick={()=>{
+        const text = window.navigator.clipboard.readText(); 
+        console.log(text);
+        }} >Read text from buffer</button>
         </>
     )
 }
