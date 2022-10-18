51e1e62b315b88f4c044b6d630242abf86dff214
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-18 10:05:35 +0300

    Formik form

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,82 +1,40 @@
 import React, { Component } from 'react';
 import { SCHEMA } from '../../schemas';
+import { Formik, Form, Field } from 'formik';
 
 
 const initialState = {
     firstName:'',
     lastName:'',
-    email: ' ',
-    pass: ' ',
+    email: '',
+    pass: '',
 }
 
 
-class SignUpForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...initialState,
-            isError: null
-        }
-    }
+function SignUpForm (props) {
 
-    changeHandler = ({target: {value, name}}) => {
-        this.setState({
-            [name]: value
-        })
-    }
-
-    submitHandler = (event) => {
-        event.preventDefault();
-
-        try {
-          const result = SCHEMA.validateSync(this.state);
-          console.log(result);
-        } catch(err) {
-            this.setState({
-                isError: err
-            })
-        }
-    }
     
-    render() {
-        const {firstName, lastName, email,pass, isError} = this.state;
+const handleSubmitToFormik = (values,actions) => {
+   actions.resetForm();
+}
+
         return (
-            <form onSubmit={this.submitHandler}>
-                <input 
-                    type="text"
-                    value={firstName}
-                    name="firstName"
-                    onChange={this.changeHandler}
-                    placeholder="firstName"
-                    />
-                    <input 
-                    type="text"
-                    value={lastName}
-                    name="lastName"
-                    onChange={this.changeHandler}
-                    placeholder="lastName"
-                    />
-                <input 
-                    type="text"
-                    value={email}
-                    name="email"
-                    onChange={this.changeHandler}
-                    placeholder="email"
-                
-                    />
-                <input 
-                    type="text" 
-                    value={pass}
-                    name="pass"
-                    onChange={this.changeHandler}
-                    placeholder="pass"
-                   
-                    />
-                    <button>Submit</button>
-                    {isError && <p style={{color: 'red', fontSize: '20px'}}>{isError.message}</p>}
-            </form>
+            <Formik 
+            initialValues={initialState}
+            onSubmit={handleSubmitToFormik}
+            >
+                {(formikProps)=>{
+                    return (
+                        <Form>
+                            <Field name="firstName" placeholder="firstName"/>
+                            <Field name="lastName" placeholder="lastName"/>
+                            <Field name="email" placeholder="email"/>
+                            <Field name="pass" placeholder="pass"/>
+                            <input type='submit' value='submit'/>
+                        </Form>)
+                }}
+            </Formik>
         );
     }
-}
 
 export default SignUpForm;
