59c1c1500a5a14f1aa69cf4480b6aaa2c7268aa7
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-19 11:59:44 +0300

    App refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import MouseTracker from "./MouseTracker";
 import Counter from './NewCounter/counter';
 import UserLoader from './UserList/UserLoader';
@@ -8,33 +8,20 @@ import CONSTANTS from '../constants';
 import { UserContext } from "../contexts/userContext";
 const {THEMES} = CONSTANTS;
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            theme: THEMES.DARK,
-            user: {
-                firstName: 'John',
-                lastName: 'Snow'
-            }
-        }
-    }
-    setTheme = (theme) => {
-        this.setState({
-            theme: theme
-        })
-    }
+function App (props) {
+    const [user, setUser] = useState({
+                        firstName: 'John',
+                        lastName: 'Snow'
+                    });
+    const [theme, setTheme] = useState(THEMES.DARK)
  
-    render(){
-        const {user, theme} = this.state;
         return (
           <UserContext.Provider value={[user, ()=>{}]}> 
-            <ThemeContext.Provider value={[theme, this.setTheme]}>
+            <ThemeContext.Provider value={[theme, setTheme]}>
                 <Tree />
             </ThemeContext.Provider>  
           </UserContext.Provider> 
         )
-    }
 }
 
 
