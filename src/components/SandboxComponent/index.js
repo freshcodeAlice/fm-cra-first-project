dd2cf3f15022bc7e510171f4ef99615f327676e2
freshcodeAlice <freshjs.ofte@gmail.com>
2022-10-20 11:03:11 +0300

    useCallback task resolve

--- src/components/SandboxComponent/index.js
+++ src/components/SandboxComponent/index.js
@@ -1,16 +1,37 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 
 const Clicker = () => {
     const [click, setClick] = useState(0);
+    const [value, setValue] = useState('');
 
    const handler = useCallback(()=>{
         setClick(click => click+1);
     }, []);
 
+   const changeHandler = ({target: {value}}) => {
+        setValue(value)
+    }
+
+    const logValue = useCallback(() => {
+            let sum = 0;
+            for(let i = 0; i<400000000; i++) {
+                sum+=i;
+            }
+                console.log(value);
+            },[value]);
+            
+
+    useEffect(()=>{
+             console.log('logValue has changed');
+    }, [logValue]);
+
+
     return (
         <>
             <h1> {click} </h1>
             <button onClick={handler}>Click</button>
+            <input type="text" value={value} onChange={changeHandler}/>
+            <button onClick={logValue}>Click to log value</button>
         </>
     );
 }
